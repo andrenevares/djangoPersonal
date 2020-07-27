05c17e4fed745693122cf286ab88bbab50d83480
andrenevares <andre.nevares@gmail.com>
2020-07-27 19:26:00 -0300

    .

--- personal_project/blog/models.py
+++ personal_project/blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    titulo = models.CharField(max_length=100)
+    chamada = models.CharField(max_length=250)
+    author = models.CharField(max_length=250)
+    link = models.URLField(blank=True)
+    imagem = models.ImageField(upload_to='portfolio/images')
+    conteudo = models.TextField()
+
