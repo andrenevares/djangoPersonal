a411ebb022d87532753b626e29d4a5cc57bf4204
andrenevares <andre.nevares@gmail.com>
2020-07-28 00:39:57 -0300

    .

--- personal_project/blog/views.py
+++ personal_project/blog/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def blog_main(request):
-    return render (request, 'blog/index.html')
+    post = {}
+    return render (request, 'blog/index.html', 
+    
+    )
--- personal_project/personal/views.py
+++ personal_project/personal/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from .models import Project
 
 def home(request):
-    return render(request, 'personal/home.html')
+    projects = Project.objects.all()
+    return render(request, 'personal/home.html', {'projects': projects})
